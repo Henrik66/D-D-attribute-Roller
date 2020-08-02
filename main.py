7088225b7ec0f3e5ce8c30577d324759c01ee404
Henrik Gombos <henrik99999@gmail.com>
2020-08-02 16:29:11 +0000

    Working version.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import random
 
-roll1 = [(random.randint(1,6))]
+def rollonetime():
+  roll1 = [(random.randint(1,6))]
+  for x in range (0, 4):
+    roll1.append(random.randint(1,6))
+  roll1.sort()
 
-for x in range (0, 3):
-  roll1.append(random.randint(1,6))
+  summary = roll1[1] + roll1[2] + roll1[3]
+  return summary
 
-roll1.sort()
+roll2=[]
+for x in range (0, 6):
+  roll2.append(rollonetime())
 
-print (roll1)
+roll2.sort()
+product = [roll2[5], roll2[4], roll2[3], roll2[2], roll2[1], roll2[0]]
+print ("D&D charater main attributes:", product)
